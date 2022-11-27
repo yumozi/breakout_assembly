b372da54a1b22ebf1f617b461da7375f1afa7f4e
Yumozi <xueeric1223@gmail.com>
2022-11-27 11:59:19 -0500

    Merge new base file

--- breakout.asm
+++ breakout.asm
@@ -57,37 +57,42 @@ ADDR_KBRD:
 main:
     # Initialize the game
  
+ # DRAW_LINE_IN_ROWS use to initialize the top row walls
+ # Data of the top wall rows: 4 UNIT high
  jal DRAW_LINE_IN_ROWS
  
- # Initialize again 
+ # Preperation for drawing the LEFT and RIGHT column walls
 	li $t0, 0
 	li $t1, 64
 	la $a0, ADDR_DSPL	
 	lw $a0, 0($a0)	#$a0 stores the first piexl address
 	li $t7, 0
-	
+
+# $t5 + 1 indicate how many columns wall to draw	
 	li $t4, 0
 	li $t5, 3
+
     	jal DRAW_LINE_IN_COLUMNS
  
 	
 	
-
+# DRAW the brick in the middle
 SETUP_DRAW_BRICK:
-	addi $t9, $0, 16 # Set $t9 to the location of the first y-coordinate of the bricks
+	# Set $t9 to the location of the first y-coordinate of the bricks
+	addi $t9, $0, 16 
 	
-	li $a0, 12	# x coordinate
-	addi $a1, $t9, 0	# y coordinate
-	la $a2, COLOR	# color of the brick
-	lw $a2, 4($a2)
+	li $a0, 12		# x coordinate of the first line brick
+	addi $a1, $t9, 0	# y coordinate of the first line brick
+	la $a2, COLOR		
+	lw $a2, 4($a2)		# color of the first brick
 	
 	jal DRAW_BRICKS
 	
-	addi $t9, $t9, 2
+	addi $t9, $t9, 2	# Draw the next line
 	
-	li $a0, 12	 # x coordinate
+	li $a0, 12	 	# x coordinate second
 	addi $a1, $t9, 0	# y coordinate
-	la $a2, COLOR	 # color of the brick
+	la $a2, COLOR	 	# color of the brick
 	lw $a2, 8($a2)
 	
 	jal DRAW_BRICKS
@@ -95,26 +100,26 @@ SETUP_DRAW_BRICK:
 	addi $t9, $t9, 2
 	
 	
-	li $a0, 12	# x coordinate
+	li $a0, 12		# x coordinate third
 	addi $a1, $t9, 0	# y coordinate
-	la $a2, COLOR	# color of the brick
+	la $a2, COLOR		# color of the brick
 	lw $a2, 12($a2)
 	
 	jal DRAW_BRICKS
 	
 	addi $t9, $t9, 2
 
-	li $a0, 12	# x coordinate
-	addi $a1, $t9, 0	# y coordinate
-	la $a2, COLOR	# color of the brick
+	li $a0, 12		# x coordinate of the fourth
+	addi $a1, $t9, 0	# y coordinate	fourth
+	la $a2, COLOR		# color of the brick
 	lw $a2, 16($a2)
 	jal DRAW_BRICKS
 	
 	addi $t9, $t9, 2
 	
-	li $a0, 12	# x coordinate
-	addi $a1, $t9, 0	# y coordinate
-	la $a2, COLOR	# color of the brick
+	li $a0, 12		# x coordinate fifth
+	addi $a1, $t9, 0	# y coordinate fifth
+	la $a2, COLOR		# color of the brick
 	lw $a2, 20($a2)
 	
 	
@@ -125,22 +130,22 @@ SETUP_DRAW_BRICK:
 INIT_PADDLE:
 	li $s0, 56	# x-coordinate of the paddle
 	li $s1, 63	# y-coodinate of the paddle
-	la $t1, COLOR
-	lw $t1, 24($t1)
-	
-	la $t0, ADDR_DSPL
-	lw $t0, 0($t0)	
-	sll $s0, $s0, 2
-	sll $s1, $s1, 9
-	add $t0, $t0, $s0
-	add $t0, $t0, $s1
-	
+
+	li $a0, 0	
+	li $a1, 0
+	add $a0, $a0, $s0
+	add $a1, $a1, $s1
+		
+# Set the offset from the base address
 	
 	li $t5, 0	# $t5, Inner loop of Draw Row
-	li $t6, 15	# $t6, the termination of inner draw
-	jal INT_PADDLE_DRAW
-	li $s0, 60	# Send $s0 back 
-	li $s1, 63	# Send $s1 back  
+	li $t6, 15	# $t6 + 1 indicate the lenght of the paddle
+	
+		
+# $a0, x coordinat of paddle, $a1, y coordinate of paddle
+	jal DRAW_PADDLE
+	
+	
 	
 INIT_BALL:
 	li $s2, 64
@@ -154,37 +159,39 @@ INIT_BALL:
 	li $s2, 64
 	li $s3, 60
 	
-	
-
-		
-DRAW_NEW_BALL:
-	li $s4, 0
-	li $s5, -1
-	
-	
-	li $a0, 0
-	li $a1, 0
-	add $a0, $a0, $s2
-	add $a1, $a1, $s3
-	jal DELETE_BALL
-	
-	li $a0, 0
-	li $a1, 0
-	add $a0, $s2, $s4
-	add $a1, $s3, $s5
-	jal DRAW_BALL
-	add $s2, $s2, $s4
-	add $s3, $s3, $s5
-	
-	
-	
-	
-
 	j END	
 
 
+
+# $a0: most left x coordinate of the paddle, $a1, most left y coordinate of the paddle
+DRAW_PADDLE:
+	la $t1, COLOR
+	lw $t1, 24($t1)	# $t1, Color of paddle
+
+	la $t0, ADDR_DSPL
+	lw $t0, 0($t0)	
 	
-		
+	sll $a0, $a0, 2
+	sll $a1, $a1, 9
+	add $t0, $t0, $a0
+	add $t0, $t0, $a1
+	
+DRAW_PADDLE_LOOP:
+	
+	slt $t7, $t5, $t6 # $t7 store the result for $t0 < $t1
+	beq $t7, $0, END_DRAW_PADDLE
+		# LOOP BODY
+		sw $t1, 0($t0)	# put color to the address
+		addi $t0, $t0, 4 # go to the next unit
+	addi $t5, $t5, 1 # i = i + 1
+	j DRAW_PADDLE_LOOP
+	
+END_DRAW_PADDLE:
+	jr $ra	
+	
+	
+
+# $a0, x coodinate of ball, $a1, y coordinate of ball, draw ball with white color 		
 DRAW_BALL:
 	la $t1, COLOR
 	lw $t1, 28($t1)
@@ -201,7 +208,8 @@ DRAW_BALL:
 	sw $t1, 0($t0) 	
 	jr $ra
 		
-						
+
+# $a0, x of ball, $a1, y of ball, draw ball with black						
 DELETE_BALL:
 	la $t1, COLOR
 	lw $t1, 32($t1)
@@ -253,9 +261,10 @@ END_DRAW_ROW_LINE:
 
 END_DRAW_ROWS: 
 	jr $ra										
-			
-DRAW_LINE_IN_COLUMNS:
 
+			
+																
+DRAW_LINE_IN_COLUMNS:
 DRAW_LINE_IN_LEFT_COLUMN:
 	slt $t3, $t0, $t1 # $t3 store the result for $t0 < $t1
 	beq $t3, $0, END_DRAW_LEFT_ROW_COLUMN # If draw done return to the END_DRAW_ROW_LINE
@@ -276,44 +285,26 @@ END_DRAW_LEFT_ROW_COLUMN:
 	slt $t6, $t4, $t5
 	beq $t6, $0, END_DRAW_LEFT_COLUMNS
 		addi $t4, $t4, 1
-		j DRAW_LINE_IN_LEFT_COLUMN
-	
-	
+		j DRAW_LINE_IN_LEFT_COLUMN	
 END_DRAW_LEFT_COLUMNS:
 	jr $ra
 	
 	
 	
-INT_PADDLE_DRAW:
-	slt $t7, $t5, $t6 # $t7 store the result for $t0 < $t1
-	beq $t7, $0, END_DRAW_LINE_BRICK 
-		# LOOP BODY
-		sw $t1, 0($t0)	# put color to the address
-		addi $t0, $t0, 4 # go to the next unit
-	addi $t5, $t5, 1 # i = i + 1
-	j INT_PADDLE_DRAW
-	
-END_INT_PADDLE_DRAW:
-	jr $ra
-	
-	
-	
-
+# $a0, x of the most left brick , $a1, y of the most left brick
 DRAW_BRICKS:
+
 	la $t0, ADDR_DSPL # $t0 is the first address of the brick
 	lw $t0, 0($t0)	
 	sll $a0, $a0, 2
 	sll $a1, $a1, 9
 	add $t0, $t0, $a0
 	add $t0, $t0, $a1
-	
-	li $t3, 0	#$t3, Outer loop of Draw Row i
+		li $t3, 0	#$t3, Outer loop of Draw Row i
 	li $t4, 1	#$t4, The termination of Outer draw row brick
 	
 	li $t5, 0	# $t5, Inner loop of Draw Row
-	li $t6, 104	# $t6, the termination of inner draw
-	
-	
+	li $t6, 104	# $t6, the termination of inner draw	
 DRAW_LINE_BRICK:
 	slt $t7, $t5, $t6 # $t3 store the result for $t0 < $t1
 	beq $t7, $0, END_DRAW_LINE_BRICK # If draw done return to the END_DRAW_ROW_LINE
@@ -330,21 +321,39 @@ END_DRAW_LINE_BRICK:
 	beq $t8, $0, END_DRAW_ONE_BRICK
 		addi $t3, $t3, 1
 		j DRAW_LINE_BRICK
-
 END_DRAW_ONE_BRICK:		
 	jr $ra
 	
-game_loop:
-	# 1a. Check if key has been pressed
-    # 1b. Check which key has been pressed
-    # 2a. Check for collisions
-	# 2b. Update locations (paddle, ball)
-	# 3. Draw the screen
-	# 4. Sleep
 
-    #5. Go back to 1
-    b game_loop
+
+	
+game_loop:
+# 1a. Check if key has been pressed
+# 1b. Check which key has been pressed
+# 2a. Check for collisions
+# 2b. Update locations (paddle, ball)
+# 3. Draw the screen
+# 4. Sleep
+# 5. Go back to 1
+	
+	lw $t0, ADDR_KBRD               # $t0 = base address for keyboard
+	lw $t8, 0($t0)                  # Load first word from keyboard
+	beq $t8, 1, on_keyboard_input   # If first word 1, key is pressed
+	b game_loop
+
+
+on_keyboard_input:                  	# A key is pressed
+	lw $a0, 4($t0)                  # Load second word from keyboard
+	beq $a0, 0x71, END     		# IF q pressed, END
+	beq $a0, 0x61, END     		# IF a pressed, move paddle left
+	beq $a0, 0x64, END 		# IF d pressed, move paddle right
+
+	li $v0, 1                       # ask system to print $a0
+	syscall
+
+	b main
+
     
-END:
-	li $v0, 10
+END:	
+	li $v0, 10			# Quit gracefully
 	syscall
